55f06864de8889e991b8d72a08bb1dbd2cc397af
ricardo <ric_vicentini@hotmail.com>
2019-12-14 11:34:47 -0300

    primeiro

---
+++ Enums/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+
+namespace Enums
+{
+    public static class PatenteV1
+    {
+        public enum Pefil
+        {
+            //Posto mais baixo do Exercíto brasileiro 
+            Graduado = 1,
+            //É o primeiro nível dos oficiais do exercíto 
+            Subalterno = 2,
+            //Nível acima dos subalternos
+            Itermediario = 3,
+            //Sáo oficiais de nível superior em relação os demais níveis
+            Superior = 4,
+            //Classe mais alta do exercíto brasileiro
+            General = 5
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lista de perfis");
+          
+            Console.WriteLine(ListarEnums());
+        }
+
+        private static StringBuilder ListarEnums()
+        {
+            var stringBuilder = new StringBuilder();
+            foreach (var perfil in Enum.GetNames(typeof(PatenteV1.Pefil)))
+            {
+                stringBuilder.AppendLine(perfil);
+            }
+
+            return stringBuilder;
+        }
+    }
+}
---
+++ EnumsComDescricao/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace EnumsComDescricao
+{
+    public static class EnumExtensions
+    {
+        public static string Descricao(this Enum enumRecebido)
+        {
+            var descriptionAttribute = enumRecebido.GetType()
+                .GetMember(enumRecebido.ToString())[0]
+                .GetCustomAttribute(typeof(DescriptionAttribute), false) as DescriptionAttribute;
+
+            return descriptionAttribute is null ? enumRecebido.ToString() : descriptionAttribute.Description;
+        }
+    }
+
+
+    public enum Perfil
+    {
+
+        [Description("Posto mais baixo do Exercíto brasileiro ")]
+        Graduado = 1,
+        [Description("É o primeiro nível dos oficiais do exercíto ")]
+        Subalterno = 2,
+        [Description("Nível acima dos subalternos")]
+        Itermediario = 3,
+        [Description("Sáo oficiais de nível superior em relação os demais níveis")]
+        Superior = 4,
+        [Description("Classe mais alta do exercíto brasileiro")]
+        General = 5,
+        Master = 6
+    }
+
+
+    class Program
+    {
+
+        
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ListarDescricoesPerfis());
+        }
+
+        private static string ListarDescricoesPerfis()
+        {
+            var stringBuilder = new StringBuilder();
+            foreach (Perfil perfil in Enum.GetValues(typeof(Perfil)))
+            {
+                stringBuilder.AppendLine($"{perfil.ToString()} - {perfil.Descricao()}");
+            }
+
+            return stringBuilder.ToString();
+        }
+        private static StringBuilder ListarPerfis()
+        {
+            var stringBuilder = new StringBuilder();
+            foreach (var perfil in Enum.GetNames(typeof(Perfil)))
+            {
+                stringBuilder.AppendLine(perfil);
+            }
+
+            return stringBuilder;
+        }
+
+    }
+}
